3d29bb3585d3b2810861e9f1025b802a5d23b728
DragonKingwhd <dragonkingwhd@gmail.com>
2026-05-06 19:12:26 +0800

    5.6学习，match case

--- Day01-20/00.test.py
+++ Day01-20/00.test.py
@@ -1,14 +1,7 @@
-Height= float(input("请输入你的身高（米）"))
-Weight= float(input("请输入你的体重（千克）"))
-BMI = Weight / (Height ** 2)
-if BMI < 18.5:
-    print("你的体重过轻")
-elif 18.5 <= BMI < 24:
-    print("你的体重正常")
-elif 24 <= BMI < 28:
-    print("你的体重过重")
-else:
-    print("你的体重肥胖")
-print(f"你的bmi是:{BMI:.1f}")
-# print(f'{bmi = :.1f}')
-# print("你的BMI是：", BMI)
+status_code = int(input("显示代码："))
+match status_code:
+    case 101: description = "不及格"
+    case 102: description = "及格"
+    case 103: description = "差一点及格"
+    case _: description = '未知代码 请联系管理员'
+print('状态码描述：',description)
